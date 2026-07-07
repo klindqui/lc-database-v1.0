a7a32da3464ff4fc5e901f466e25996858350112
klindqui <katlindquist6282@gmail.com>
2026-07-06 22:19:42 -0400

    added new accounts

--- schema/public/roles/user.sql
+++ schema/public/roles/user.sql
@@ -20,3 +20,51 @@ WITH LOGIN
 PASSWORD 'placeholderpass';
 
 GRANT db_admin to ericaholswade
+
+REVOKE db_admin FROM ericaholswade;
+GRANT data_analyst TO ericaholswade;
+
+-- OTHER USERS
+
+-- charles here
+CREATE USER cbarry
+WITH LOGIN
+PASSWORD 'placeholderpass';
+GRANT data_analyst TO cbarry;
+
+-- uncle 
+CREATE USER rlindquist
+WITH LOGIN
+PASSWORD 'placeholderpass';
+GRANT data_analyst TO rlindquist;
+
+-- marc
+CREATE USER mweiss
+WITH LOGIN
+PASSWORD 'placeholderpass';
+GRANT data_analyst TO mweiss;
+
+-- mark
+CREATE USER mkrikorian
+WITH LOGIN
+PASSWORD 'placeholderpass';
+GRANT data_analyst TO mkrikorian;
+
+-- glenn
+CREATE USER ghitchcock
+WITH LOGIN
+PASSWORD 'placeholderpass';
+GRANT data_analyst TO ghitchcock;
+
+-- sarah
+CREATE USER swilliams
+WITH LOGIN
+PASSWORD 'placeholderpass';
+GRANT data_analyst TO swilliams;
+
+-- ganesh
+CREATE USER gnehru
+WITH LOGIN
+PASSWORD 'placeholderpass';
+GRANT data_analyst TO gnehru;
+
